6080ec5a777a8d84a57fe7990dc46ae507c5e1d5
smiley <smiley@chillerlan.net>
2017-10-22 08:27:43 +0200

    :sparkles: discord

---
+++ examples/get-token/discord.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @filesource   discord.php
+ * @created      22.10.2017
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2017 Smiley
+ * @license      MIT
+ */
+
+use chillerlan\OAuth\Providers\Discord;
+
+require_once __DIR__.'/../bootstrap.php';
+
+$scopes = [
+#	Discord::SCOPE_BOT,
+	Discord::SCOPE_CONNECTIONS,
+	Discord::SCOPE_EMAIL,
+	Discord::SCOPE_IDENTIFY,
+	Discord::SCOPE_GUILDS,
+	Discord::SCOPE_GUILDS_JOIN,
+	Discord::SCOPE_GDM_JOIN,
+	Discord::SCOPE_MESSAGES_READ,
+#	Discord::SCOPE_RPC,
+	Discord::SCOPE_RPC_API,
+	Discord::SCOPE_RPC_NOTIFICATIONS_READ,
+#	Discord::SCOPE_WEBHOOK_INCOMING,
+];
+
+/** @var \chillerlan\OAuth\Providers\Discord $provider */
+$provider = getProvider('Discord', $scopes);
+
+if(isset($_GET['login']) && $_GET['login'] === $provider->serviceName){
+	header('Location: '.$provider->getAuthURL());
+}
+elseif(isset($_GET['code']) && isset($_GET['state'])){
+	$token = $provider->getAccessToken($_GET['code'], $_GET['state']);
+
+	// save the token & redirect
+	saveToken($token, $provider->serviceName);
+}
+elseif(isset($_GET['granted']) && $_GET['granted'] === $provider->serviceName){
+	echo '<pre>'.print_r($provider->request('/users/@me')->json,true).'</pre>';
+}
+else{
+	echo '<a href="?login='.$provider->serviceName.'">Login with '.$provider->serviceName.'!</a>';
+}
+
+exit;
+
+
+
+
+
---
+++ src/Providers/Discord.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Class Discord
+ *
+ * @filesource   Discord.php
+ * @created      22.10.2017
+ * @package      chillerlan\OAuth\Providers
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2017 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\OAuth\Providers;
+
+/**
+ * @link https://discordapp.com/developers/docs/topics/oauth2
+ */
+class Discord extends OAuth2Provider{
+
+	const SCOPE_BOT                    = 'bot';
+	const SCOPE_CONNECTIONS            = 'connections';
+	const SCOPE_EMAIL                  = 'email';
+	const SCOPE_IDENTIFY               = 'identify';
+	const SCOPE_GUILDS                 = 'guilds';
+	const SCOPE_GUILDS_JOIN            = 'guilds.join';
+	const SCOPE_GDM_JOIN               = 'gdm.join';
+	const SCOPE_MESSAGES_READ          = 'messages.read';
+	const SCOPE_RPC                    = 'rpc';
+	const SCOPE_RPC_API                = 'rpc.api';
+	const SCOPE_RPC_NOTIFICATIONS_READ = 'rpc.notifications.read';
+	const SCOPE_WEBHOOK_INCOMING       = 'webhook.incoming';
+
+	protected $apiURL              = 'https://discordapp.com/api/v6';
+	protected $authURL             = 'https://discordapp.com/api/oauth2/authorize';
+	protected $userRevokeURL       = 'https://discordapp.com/api/oauth2/token/revoke';
+	protected $accessTokenEndpoint = 'https://discordapp.com/api/oauth2/token';
+	protected $accessTokenExpires  = true;
+	protected $checkState          = true;
+	protected $authMethod          = self::HEADER_BEARER;
+
+	/**
+	 * @link https://discordapp.com/developers/docs/topics/oauth2#client-credentials-grant
+	 *
+	 * @param array $scopes
+	 *
+	 * @return \chillerlan\OAuth\Token
+	 * @throws \chillerlan\OAuth\OAuthException
+	 */
+	public function requestCredentialsToken(array $scopes = []){
+
+		$token = $this->parseResponse(
+			$this->http->request(
+				$this->accessTokenEndpoint,
+				[],
+				'POST',
+				[
+					'grant_type'    => 'client_credentials',
+					'scope'         => implode($this->scopesDelimiter, $scopes),
+				],
+				[
+					'Authorization' => 'Basic '.base64_encode($this->options->key.':'.$this->options->secret),
+				]
+			)
+		);
+
+		$this->storage->storeAccessToken($this->serviceName, $token);
+
+		return $token;
+	}
+
+}
