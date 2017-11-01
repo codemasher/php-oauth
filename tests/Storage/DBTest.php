6a9167a7f1e99580ac22180852852f73206410cc
smiley <smiley@chillerlan.net>
2017-11-01 10:29:47 +0100

    :octocat: use test storage

--- tests/Storage/DBTest.php
+++ tests/Storage/DBTest.php
@@ -11,41 +11,9 @@
 
 namespace chillerlan\OAuthTest\Storage;
 
-use chillerlan\Database\{
-	Connection, Options, Drivers\Native\MySQLiDriver, Query\Dialects\MySQLQueryBuilder
-};
-
-use chillerlan\OAuth\{
-	Storage\DBTokenStorage, Token
-};
-
-use Dotenv\Dotenv;
-
 class DBTest extends TokenStorageTestAbstract{
-	const CFGDIR = __DIR__.'/../../config';
-	const TABLE_TOKEN    = 'storagetest';
-	const TABLE_PROVIDER = 'storagetest_providers';
 
-	protected $FQCN = DBTokenStorage::class;
-
-	public function setUp(){
-		$env = file_exists(self::CFGDIR.'/.env') ? '.env' : '.env_travis'; // ugh
-
-		(new Dotenv(self::CFGDIR, $env))->load();
-
-		$db = new Connection(new Options([
-			'driver'       => MySQLiDriver::class,
-			'querybuilder' => MySQLQueryBuilder::class,
-			'host'         => getenv('MYSQL_HOST'),
-			'port'         => getenv('MYSQL_PORT'),
-			'database'     => getenv('MYSQL_DATABASE'),
-			'username'     => getenv('MYSQL_USERNAME'),
-			'password'     => getenv('MYSQL_PASSWORD'),
-		]));
-
-		$this->storage = new DBTokenStorage($db, self::TABLE_TOKEN, self::TABLE_PROVIDER, 1);
-		$this->token   = new Token(['accessToken' => 'foobar']);
-	}
+	protected $FQCN = TestDBStorage::class;
 
 	/**
 	 * @expectedException \chillerlan\OAuth\OAuthException
