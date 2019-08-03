1feaa2ee073f009150055f4d94e4a57533e36ed9
codemasher <smiley@chillerlan.net>
2019-08-03 03:13:01 +0200

    :sparkles: Spotify example

---
+++ examples/Spotify/spotify-common.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @filesource   spotify-common.php
+ * @created      03.08.2019
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+use chillerlan\OAuth\Core\AccessToken;
+use chillerlan\OAuth\Providers\Spotify\Spotify;
+
+$ENVVAR = 'SPOTIFY';
+$CFGDIR = null;
+
+/** @var \chillerlan\Settings\SettingsContainerInterface $options */
+$options = null;
+
+/** @var \Psr\Log\LoggerInterface $logger */
+$logger = null;
+
+/** @var \Psr\Http\Client\ClientInterface $http */
+$http = null;
+
+/** @var \chillerlan\OAuth\Storage\OAuthStorageInterface $storage */
+$storage = null;
+
+require_once __DIR__.'/../provider-example-common.php';
+
+if(!$storage->hasAccessToken('Spotify')){
+	$storage->storeAccessToken('Spotify', (new AccessToken)->fromJSON(file_get_contents($CFGDIR.'/Spotify.token.json')));
+}
+
+$spotify = new Spotify($http, $storage, $options, $logger);
---
+++ examples/provider-example-common.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @filesource   provider-example-common.php
+ * @created      26.07.2019
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\OAuthExamples;
+
+use chillerlan\Database\{Database, Drivers\MySQLiDrv};
+use chillerlan\OAuthApp\{OAuthAppOptions, Storage\DBStorage};
+use chillerlan\SimpleCache\MemoryCache;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$CFGDIR = __DIR__.'/../config';
+
+/** @var \chillerlan\DotEnv\DotEnv $env */
+$env = null;
+
+/** @var \Psr\Log\LoggerInterface $logger */
+$logger = null;
+
+require_once __DIR__.'/../vendor/chillerlan/php-oauth-core/examples/oauth-example-common.php';
+
+$dboptions = new OAuthAppOptions([
+	'driver'      => MySQLiDrv::class,
+	'host'        => $env->DB_HOST,
+	'port'        => $env->DB_PORT,
+	'socket'      => $env->DB_SOCKET,
+	'database'    => $env->DB_DATABASE,
+	'username'    => $env->DB_USERNAME,
+	'password'    => $env->DB_PASSWORD,
+
+	'db_table_provider' => 'oauth_providers',
+	'db_table_token'    => 'oauth_tokens',
+	'db_user_id'        => 1,
+	'storageEncryption' => true,
+	'storageCryptoKey'  => '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f'
+]);
+
+$db = new Database($dboptions, new MemoryCache, $logger);
+$db->connect();
+
+$storage = new DBStorage($db, $dboptions, $logger);
