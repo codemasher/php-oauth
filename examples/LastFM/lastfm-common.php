06bc404ccb48ad164faf98479114179bfb42fb0a
codemasher <smiley@chillerlan.net>
2019-08-04 15:05:07 +0200

    :sparkles: Last.fm example

---
+++ examples/LastFM/lastfm-common.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @filesource   lastfm-common.php
+ * @created      03.08.2019
+ * @author       smiley <smiley@chillerlan.net>
+ * @copyright    2019 smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\OAuthAppExamples\LastFM;
+
+use chillerlan\OAuth\Core\AccessToken;
+use chillerlan\OAuth\Providers\LastFM\LastFM;
+
+$ENVVAR = 'LASTFM';
+$CFGDIR = null;
+
+/** @var \chillerlan\Settings\SettingsContainerInterface $options */
+$options = null;
+
+/** @var \Psr\Log\LoggerInterface $logger */
+$logger = null;
+
+/** @var \Psr\Http\Client\ClientInterface $http */
+$http = null;
+
+/** @var \chillerlan\OAuth\Storage\OAuthStorageInterface $storage */
+$storage = null;
+
+require_once __DIR__.'/../provider-example-common.php';
+
+if(!$storage->hasAccessToken('LastFM')){
+	$storage->storeAccessToken('LastFM', (new AccessToken)->fromJSON(file_get_contents($CFGDIR.'/LastFM.token.json')));
+}
+
+$lfm = new LastFM($http, $storage, $options, $logger);
